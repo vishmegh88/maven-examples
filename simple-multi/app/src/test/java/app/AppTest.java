74d1f0ef6067991fc39d57b1b2ac009d75267374
vmeghani <vmeghani@salesforce.com>
2019-01-07 18:41:48 +0530

    adding imports to runner

--- simple-multi/app/src/test/java/app/AppTest.java
+++ simple-multi/app/src/test/java/app/AppTest.java
@@ -3,6 +3,8 @@ package app;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.walkmod.junit4git.junit4.Junit4GitRunner;
 
 @RunWith(Junit4GitRunner.class)
 public class AppTest {
